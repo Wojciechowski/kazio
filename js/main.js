739c54eb406f02c1e859b3e88cb0bfc5866d9a5c
Piotr Wojciechowski <piotr.wojciechowski@passus.pl>
2014-03-06 15:40:00 +0100

    start sponsorzy

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
 
     /* Top banner Sponsors */
-    var sponsor;
+    var sponsors = $('.sponsors'),
+        sponsor,
+        padd = 10,
+        i = 0;
 
-    $('.sponsors').children().each(function(){
+    sponsors.children().each(function(){
         if (!$(this).is('ul')) {
             sponsor = $(this).text();
             console.log(sponsor);
@@ -14,5 +17,14 @@ $(document).ready(function() {
             }
         }
     })
+    $('.imagemask', sponsors).each(function(){ // zamienić na 'img'
+        $(this).parent().css('padding-left', ($(this).outerWidth() + padd) + 'px');
+        $(this).wrap('<span class="brand"/>');
+    })
+    if (!$('.active', sponsors).is('li')) {
+        sponsor = $('li:first-of-type').addClass('active').parent().attr('data-sponsor');
+        $('.spons-h').html(sponsor + ':');
+    }
+
 //    $('.navbar-header').collapse();
 });
