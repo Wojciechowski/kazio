a89778661c900075bea62eadda944fb123a046e6
Piotr Wojciechowski <piotr.wojciewchowski@kontakt.waw.pl>
2014-04-21 13:22:50 +0200

    top baner

--- global/js/main.js
+++ global/js/main.js
@@ -6,54 +6,47 @@
  * create: 04-03-2014
  */
 
-steal(
-    'jquery.min.js',
-    'widgets/sponsors.js',
-    'widgets/sponsor_b.js',
-function(jQuery){
-    steal(
-        'bootstrap/collapse.js'
-    )
-    /**
-     * Top banner xx
-     */
-    $.fn.xx = function(param) {
+/**
+ * Top banner xx
+ */
+$.fn.xx = function(param) {
 
-        return this.each(function(){
-            that = $(this);
-            init();
-        });
-    };
+    return this.each(function(){
+        that = $(this);
+        init();
+    });
+};
 
-    $(document).ready(function() {
-        $('#mail a').click(function(){
-            $('#mail .more').addClass('hide');
-            $('#mail .text').removeClass('hide');
-            return false;
-        })
+$(document).ready(function() {
+    $('#mail a').click(function(){
+        $('#mail .more').addClass('hide');
+        $('#mail .text').removeClass('hide');
+        return false;
+    })
+    $('.col-right').append('<div id="log"/>');
+    $(window).resize(function(){
+        $('#log').html($(this).width());
+    })
 //        $('.sponsors').Sponsors();
 
-        var im = $('#slideshow img');
-        l = im.length,
-            i = 0;
-        //im.css({'position':'absolute'});
-        im.eq(0).css({'top':0});
-        function rotor(){
-            var j = i + 1;
-            if (j == l) j = 0;
-            im.eq(j).css({'top':133});
-            im.eq(i).animate({
-                'top': -133
-            },500);
-            im.eq(j).animate({
-                'top': 0
-            },500);
-            i++;
-            if (i == l) i = 0;
-            xx=setTimeout(rotor,3000);
-        }
-        rotor();
-    });
-
-})
-
+    /*var im = $('#slideshow img');
+    l = im.length,
+        i = 0;
+    //im.css({'position':'absolute'});
+    im.eq(0).css({'top':0});
+    function rotor(){
+        var j = i + 1;
+        if (j == l) j = 0;
+        im.eq(j).css({'top':133});
+        im.eq(i).animate({
+            'top': -133
+        },500);
+        im.eq(j).animate({
+            'top': 0
+        },500);
+        i++;
+        if (i == l) i = 0;
+        xx=setTimeout(rotor,3000);
+    }
+    rotor();*/
+});
--- global/js/widgets/sponsors.js
+++ global/js/widgets/sponsors.js
@@ -6,116 +6,136 @@
  * create: 04-03-2014
  */
 
-steal('jquery.min.js', function(jQuery){
-    /**
-     * Top banner Sponsors
-     */
-    $.fn.Sponsors = function(param) {
-        param = {
-            padd: 10,
-            timer: 800,
-            pause: 2000
-        }
+/**
+ * Top banner Sponsors
+ */
+$.fn.Sponsors = function(param) {
+    param = {
+        padd: 10,
+        timer: 800,
+        pause: 2000,
+        w_start: 45
+    }
 
-        var that, ul, tim, height,
+    var that, ul, tim, height,
+        action = 0,
 
-            move2 = function(o){
-                o.css({'margin-top': height * -1});
-                if ($('.active', o).css('padding-left') == param.padd + 'px') {
-                    $('.active', o).css('padding-left', $('.active img', o).width() + param.padd);
-                }
-                o.animate({
-                    'margin-top': 0
-                }, param.timer, function(){
-                    tim = setTimeout(rotor, param.pause);
-                });
-            },
+        move2 = function(o){
+            o.css({'margin-top': height * -1});
+            if ($('.active', o).css('padding-left') == param.padd + 'px') {
+                $('.active', o).css('padding-left', $('.active img', o).width() + param.padd);
+            }
+            o.animate({
+                'margin-top': 0
+            }, param.timer, function(){
+                tim = setTimeout(rotor, param.pause);
+            });
+        },
 
-            rotor = function(){
-                var ul = $('ul', that),
-                    activ = $('.active', ul),
-                    next = activ.next(),
-                    parent = activ.parent();
+        rotor = function(){
+            var ul = $('ul', that),
+                activ = $('.active', ul),
+                next = activ.next(),
+                parent = activ.parent();
 
-                height = ul.height();
-                parent.animate({
-                    'margin-top': height
-                }, param.timer, function(){
-                    activ.removeClass('active');
-                    if (next.is('li')) {
-                        next.addClass('active');
-                        move2(parent);
-                    } else {
-                        var span = $('.spons-h');
+            height = ul.height();
+            parent.animate({
+                'margin-top': height
+            }, param.timer, function(){
+                activ.removeClass('active');
+                if (next.is('li')) {
+                    next.addClass('active');
+                    move2(parent);
+                } else {
+                    var span = $('.spons-h');
 
-                        parent = ul.eq(ul.index(parent) + 1);
-                        if (!parent.is('ul')) {
-                            parent = ul.eq(0);
-                        }
-                        next = $('li:first-child', parent);
-                        parent.css({'margin-top': height * -1});
-                        next.addClass('active');
-                        span.animate({
-                            'margin-top': height
-                        }, param.timer, function(){
-                            span.css({'margin-top': height * -1}).html(parent.attr('data-sponsor')).animate({
-                                'margin-top': 0
-                            }, param.timer, function(){
-                                move2(parent);
-                            });
-                        });
-
+                    parent = ul.eq(ul.index(parent) + 1);
+                    if (!parent.is('ul')) {
+                        parent = ul.eq(0);
                     }
-                });
-            },
-
-            init = function(){
-                    ul = $('ul', that);
-
-                    var sponsor,
-                        img = $('img', ul),
-                        i = 0,
-
-                        brand = function(){
-                            img.each(function(){
-                                if (img.index($(this)) == 0 && $(this).outerWidth() == 0) {
-                                    setTimeout(brand, 50);
-                                    return;
-                                }
-                                $(this).parent().css('padding-left', ($(this).outerWidth() + param.padd) + 'px');
-                                $(this).wrap('<span class="brand"/>');
-                            })
-                        };
-
-                    $('.sponsors-li', that).children().each(function(){
-                        if (!$(this).is('ul')) {
-                            sponsor = $(this).text();
-                        } else {
-                            $(this).attr('data-sponsor', sponsor);
-                            if ($('.active', this).is('li')) {
-                                $('.spons-h').html(sponsor);
-                            }
-                        }
+                    next = $('li:first-child', parent);
+                    parent.css({'margin-top': height * -1});
+                    next.addClass('active');
+                    span.animate({
+                        'margin-top': height
+                    }, param.timer, function(){
+                        span.css({'margin-top': height * -1}).html(parent.attr('data-sponsor')).animate({
+                            'margin-top': 0
+                        }, param.timer, function(){
+                            move2(parent);
+                        });
                     });
-                    brand();
-                    if (!$('.active', ul).is('li')) {
-                        sponsor = $('ul:first-of-type li:first', that).addClass('active').parent().attr('data-sponsor');
+
+                }
+            });
+        },
+
+        init = function(){
+            ul = $('ul', that);
+
+            var sponsor,
+                img = $('img', ul),
+                i = 0,
+
+                brand = function(){
+                    img.each(function(){
+                        if (img.index($(this)) == 0 && $(this).outerWidth() == 0) {
+                            setTimeout(brand, 50);
+                            return;
+                        }
+                        $(this).parent().css('padding-left', ($(this).outerWidth() + param.padd) + 'px');
+                        $(this).wrap('<span class="brand"/>');
+                    })
+                };
+
+            $('.sponsors-li', that).children().each(function(){
+                if (!$(this).is('ul')) {
+                    sponsor = $(this).text();
+                } else {
+                    $(this).attr('data-sponsor', sponsor);
+                    if ($('.active', this).is('li')) {
                         $('.spons-h').html(sponsor);
                     }
+                }
+            });
+            brand();
+            if (!$('.active', ul).is('li')) {
+                sponsor = $('ul:first-of-type li:first', that).addClass('active').parent().attr('data-sponsor');
+                $('.spons-h').html(sponsor);
+            }
 
-                tim = setTimeout(rotor, 1000);
+            start();
 
-            };
+            $(window).resize(function(){
+                start();
+            });
+        },
 
-        return this.each(function(){
-                that = $(this);
-                init();
-        });
-    };
+        start = function(){
+            var height = $('.sponsors-li', that).innerHeight();
 
-    $(document).ready(function() {
-        $('.sponsors').Sponsors();
+            console.log('t ', (height > 0 && height < param.w_start));
+            if (height > 0 && height < param.w_start) {
+                if (action == 0) {
+                    console.log('start');
+                    tim = setTimeout(rotor, 1000);
+                    action++;
+                }
+            } else {
+                console.log('stop');
+                $('ul', that).stop(true, true);
+                tim = undefined;
+                action = 0;
+            }
+        };
+
+
+    return this.each(function(){
+        that = $(this);
+        init();
     });
+};
 
-})
-
+$(document).ready(function() {
+    $('.sponsors').Sponsors();
+});
